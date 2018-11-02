98bc6e5bbc77688d42ea5d7cbc72ec0e9c7e4259
Lukyanov Ivan <ilukyanov@ozon.ru>
2018-11-02 19:01:08 +0300

    changes in main.go

--- main.go
+++ main.go
@@ -5,4 +5,11 @@ import "fmt"
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Printf("This is golang test\n")
+	fmt.Print(sum(1, 2), "\n")
+	result := sum(12, 2)
+	fmt.Printf("%v\n", result)
+}
+
+func sum(a int, b int) int {
+	return a + b
 }
