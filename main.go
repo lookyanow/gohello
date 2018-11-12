100dbc60e0a8db6c6dd334e3c552af5e8ac720f1
Ivan Lookyanow <ilukyanov@gitlab.ivanlook.ru>
2018-11-12 18:46:26 +0300

    new mul function added

--- main.go
+++ main.go
@@ -8,8 +8,13 @@ func main() {
 	fmt.Print(sum(1, 2), "\n")
 	result := sum(12, 2)
 	fmt.Printf("%v\n", result)
+	fmt.Printf("%v\n", mul(2,2))
 }
 
 func sum(a int, b int) int {
 	return a + b
+}
+
+func mul(a int, b int) int {
+	return a * b
 }
