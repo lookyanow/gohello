9123054d34a6a5fd159aaac598f4c8ef225e58a0
Ivan Lookyanow <lookyanow@gmail.com>
2018-11-13 20:22:38 +0300

    Added
    new sum(2,4)
    new sum(2,5)
    new sum(2,6)

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Printf("%v\n", sum(2,3))
+	fmt.Printf("%v\n", sum(2,4))
 	fmt.Printf("%v\n", mul(2,2))
+	fmt.Printf("%v\n", mul(2,5))
+	fmt.Printf("%v\n", mul(2,6))
 }
 
 func sum(a int, b int) int {
