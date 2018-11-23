4afa5cc697730a18be93ffd3265c4d33778b2e80
Lukyanov Ivan <ilukyanov@ozon.ru>
2018-11-23 17:54:53 +0300

    .idea added to gitignore

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Hello, world.\n")
-<<<<<<< HEAD
 	fmt.Printf("This is golang test\n")
 	fmt.Print(sum(1, 2), "\n")
 	result := sum(3, 2)
 	fmt.Printf("%v\n", result)
+	fmt.Printf("%d", mul(2,2))
 }
 
 func sum(a int, b int) int {
