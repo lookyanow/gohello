ff36808a0e8f880179abfa836d6006e4261ca91b
Lukyanov Ivan <ilukyanov@ozon.ru>
2018-11-23 19:34:32 +0300

    bool types

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+)
 
 func main() {
 	fmt.Printf("Hello, world.\n")
@@ -8,7 +11,14 @@ func main() {
 	fmt.Print(sum(1, 2), "\n")
 	result := sum(3, 2)
 	fmt.Printf("%v\n", result)
-	fmt.Printf("%d", mul(2,2))
+	fmt.Printf("%d\n", mul(2,2))
+	var res bool = false
+	var str string
+
+	str = os.Args[1]
+	res = str == "close" || str == "merge"
+	fmt.Printf("%v\n", res)
+
 }
 
 func sum(a int, b int) int {
